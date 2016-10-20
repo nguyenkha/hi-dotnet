1d55d1552fbe431d06bb747181f31aa0e844d414
Kha Do <donguyenkha@gmail.com>
2016-10-20 09:57:57 +0700

    Add ASP Core

---
+++ 3-ASP/Program.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebApplication
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
+    }
+  }
+}
